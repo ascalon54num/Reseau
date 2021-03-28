8fd06fdb5ad7f8bae47cd7d9251a5fe4a9837199
Stephane <stephane.mairesse54@gmail.com>
2021-03-28 10:07:33 +0200

    multithread en place

--- src/Server.java
+++ src/Server.java
@@ -11,50 +11,20 @@ import java.util.regex.Pattern;
 
 
 public class Server{
-	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-		ServerSocket server = new ServerSocket(80);
-
-		System.out.println("Démarrage du serveur sur 127.0.0.1:80.\r\nAttente d'une connexion...");
-		while (true) {
-			Socket client = server.accept();
-
-			System.out.println("Un client s'est connecté.");
-			InputStream in = client.getInputStream();
-
-			OutputStream out = client.getOutputStream();
-			Scanner sc = new Scanner(in, "UTF-8");
-			// translate bytes of request to string
-			String data = sc.useDelimiter("\\r\\n\\r\\n").next();
-
-			Matcher get = Pattern.compile("^GET").matcher(data);
-			Matcher post = Pattern.compile("^POST").matcher(data);
-
-			if (get.find()) {
-				 Matcher match = Pattern.compile("//(.*)/").matcher(data);
-				 match.find();
-				 MatchResult mr = match.toMatchResult();
-				 try {
-					System.out.println("Requête Get sur "+ mr.group(1));
-				 } catch (Exception e) {
-					 System.out.println("Requête option");
-				 }
-				byte[] response = ("HTTP/1.1 200 Ok\r\n"
-						+ "Content-Type:text/html; charset=utf-8\r\n"
-						+ "\r\n\r\n"
-						+ "<!DOCTYPE html>" 
-						+ "<html>" 
-						+ "<p>Hello World<p>" 
-						+ "</html>\r\n")
-								.getBytes("UTF-8");
-				out.write(response, 0, response.length);
-			}
-
-			if (post.find()) {
-
-			}
-			sc.close();
-			client.close();
-		}
+	public static void main(String[] args) throws IOException {
+		int port = 80;
 		
+		try (ServerSocket server = new ServerSocket(port);){
+			System.out.println("[Serveur] Démarrage du serveur sur 127.0.0.1:"+port+".\r\n");
+			while (true) {
+				System.out.println("[Serveur] Attente d'une connexion...\r\n");
+				Socket client = server.accept();
+				String clientIp = client.getInetAddress().toString();
+				int clientPort = client.getPort();
+				System.out.format("[Serveur] : Arrivée Client IP %s sur %d%n", clientIp, clientPort);
+				System.out.format("[Serveur ]: Creation du thread T_%d%n", clientPort);
+				new Thread(new ServiceClient(client, "T_" + clientPort)).start();
+			}
+		}
 	}
 }
---
+++ src/ServiceClient.java
@@ -0,0 +1,100 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ServiceClient implements Runnable {
+
+    private Socket connection;
+    private String id;
+
+    public ServiceClient(Socket laConnection, String mid)
+    {
+	    connection= laConnection;
+	    id=mid;
+	    System.out.format("Thread T__%s créé pour traiter la connection%n",id);  
+    }
+
+    private  void closeConnection(){
+        try{
+            if (connection != null)      
+            {
+                System.out.format("Fermeture connection pour %s%n", id); 
+                connection.close();
+            } 
+        }
+        catch (IOException e) {
+            System.out.format("Terminaison pour %s%n", id);
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        InputStream in = initInputStream();
+
+        OutputStream out = initOutputStream();
+        Scanner sc = new Scanner(in, StandardCharsets.UTF_8);
+        // translate bytes of request to string
+        String data = sc.useDelimiter("\\r\\n\\r\\n").next();
+
+        Matcher get = Pattern.compile("^GET").matcher(data);
+        Matcher post = Pattern.compile("^POST").matcher(data);
+
+        if (get.find()) {
+             Matcher match = Pattern.compile("//(.*)/").matcher(data);
+             match.find();
+             MatchResult mr = match.toMatchResult();
+             try {
+                System.out.println("Requête Get sur "+ mr.group(1));
+             } catch (Exception e) {
+                 System.out.println("Requête option");
+             }
+            byte[] response = ("HTTP/1.1 200 Ok\r\n"
+                    + "Content-Type:text/html; charset=utf-8\r\n"
+                    + "\r\n\r\n"
+                    + "<!DOCTYPE html>" 
+                    + "<html>" 
+                    + "<p>Hello World<p>" 
+                    + "</html>\r\n")
+                            .getBytes(StandardCharsets.UTF_8);
+            try {
+                out.write(response, 0, response.length);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (post.find()) {
+
+        }
+        sc.close();
+        closeConnection();
+    }
+
+    private OutputStream initOutputStream() {
+        OutputStream res = null;
+        try {
+            res=connection.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    private InputStream initInputStream() {
+        InputStream res = null;
+        try {
+           res = connection.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+    
+}
